66caaa2ff17740b870eb6ee29d84b24c1bf03e51
Alex Elkin <alelkdev@gmail.com>
2022-11-16 20:39:19 +0300

    test(converter): Add psalm part converter unit tests.

--- scala/library-manager/src/test/scala/xml_support/PsalmXmlConverterTest.scala
+++ scala/library-manager/src/test/scala/xml_support/PsalmXmlConverterTest.scala
@@ -1,12 +1,12 @@
 package com.alelk.pws.library_manager
 package xml_support
 
-import model.{Psalm, PsalmNumber}
+import model.{Psalm, PsalmNumber, Tonality}
 
 import advxml.data.ValidatedNelThrow
 import advxml.implicits.*
 import cats.data.Validated.Valid
-import com.github.dwickern.macros.NameOf._
+import com.github.dwickern.macros.NameOf.*
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.*
 
@@ -19,6 +19,13 @@ class PsalmXmlConverterTest extends AnyFlatSpec with should.Matchers with PsalmX
           <number edition="PV3300">100</number>
           <number edition="PV1000">220</number>
         </numbers>
+        <tonalities>
+          <tonality>a major</tonality>
+          <tonality>C major</tonality>
+        </tonalities>
+        <author>Author Name</author>
+        <translator>Translator Name</translator>
+        <composer>Composer Name</composer>
       </psalm>
 
     val Valid(psalm) = xml.decode[Psalm]
@@ -30,11 +37,28 @@ class PsalmXmlConverterTest extends AnyFlatSpec with should.Matchers with PsalmX
     number1.number shouldBe 100
     number2.bookEdition shouldBe "PV1000"
     number2.number shouldBe 220
+    psalm.tonalities should have length 2
+    val List(tonality1, tonality2) = psalm.tonalities
+    tonality1.value shouldBe "a major"
+    tonality2.value shouldBe "C major"
+    psalm.author shouldBe Some("Author Name")
+    psalm.translator shouldBe Some("Translator Name")
+    psalm.composer shouldBe Some("Composer Name")
   }
 
   nameOf(psalmEncoder) should "serialize psalm to XML" in {
     val actual =
-      Psalm("1.2.0", "Psalm Name", List(PsalmNumber("PV3300", 123), PsalmNumber("PV2000", 10))).encode.normalize
+      Psalm(
+        version = "1.2.0",
+        name = "Psalm Name",
+        numbers = List(PsalmNumber("PV3300", 123), PsalmNumber("PV2000", 10)),
+        tonalities = List(Tonality("a-major"), Tonality("c-major")),
+        author = Some("Author"),
+        translator = Some("Translator"),
+        composer = Some("Composer")
+      ).encode
+
+    println(actual)
 
     val expected =
       <psalm version="1.2.0" name="Psalm Name">
@@ -42,12 +66,27 @@ class PsalmXmlConverterTest extends AnyFlatSpec with should.Matchers with PsalmX
           <number edition="PV3300">123</number>
           <number edition="PV2000">10</number>
         </numbers>
+        <tonalities>
+          <tonality>a-major</tonality>
+          <tonality>c-major</tonality>
+        </tonalities>
+        <author>Author</author>
+        <translator>Translator</translator>
+        <composer>Composer</composer>
       </psalm>
     actual.normalize.toString() shouldBe expected.normalize.toString()
   }
 
   classOf[PsalmXmlConverter].getSimpleName should "convert Psalm to XML and parse it back" in {
-    val expected = Psalm("1.1.1", "Name", List(PsalmNumber("PV110", 1), PsalmNumber("PV2", 2)))
+    val expected =
+      Psalm(
+        version = "1.1.1",
+        name = "Name",
+        numbers = List(PsalmNumber("PV110", 1), PsalmNumber("PV2", 2)),
+        tonalities = List(Tonality("A-major"), Tonality("c-minor")),
+        author = Some("Author"),
+        translator = Some("translator"),
+        composer = Some("Composer"))
     val Valid(actual) = expected.encode.normalize.decode[Psalm]
     actual shouldBe expected
   }
