35bfcf407032e7fc026162b8c24b5367e4d54123
Alex Elkin <alelkdev@gmail.com>
2022-11-16 14:45:17 +0300

    feat(model): Add psalm & psalm number model.

---
+++ scala/library-manager/src/main/scala/model/Psalm.scala
@@ -0,0 +1,6 @@
+package com.alelk.pws.library_manager
+package model
+
+case class Psalm(version: String,
+                 name: String,
+                 numbers: List[PsalmNumber])
---
+++ scala/library-manager/src/main/scala/model/PsalmNumber.scala
@@ -0,0 +1,5 @@
+package com.alelk.pws.library_manager
+package model
+
+case class PsalmNumber(bookEdition: String,
+                       number: Int)
