9c55e7b04463c55182fa2a39916bae8775005ba3
Alex Elkin <alelkdev@gmail.com>
2022-11-17 20:01:38 +0300

    feat(model): add book model

---
+++ scala/library-manager/src/main/scala/model/Book.scala
@@ -0,0 +1,19 @@
+package com.alelk.pws.library_manager
+package model
+
+import io.lemonlabs.uri.RelativeUrl
+
+import java.util.Locale
+
+case class Book(version: String,
+                name: String,
+                language: Locale,
+                displayName: String,
+                displayShortName: String,
+                edition: String,
+                psalmRefs: List[RelativeUrl],
+                releaseDate: Option[String] = None,
+                description: Option[String] = None,
+                preface: Option[String] = None,
+                creators: List[String] = Nil,
+                editors: List[String] = Nil)
