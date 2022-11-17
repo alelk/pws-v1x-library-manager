d5e1b0c9d9695f2f4d9302e1c78d3b4a5c974d2e
Alex Elkin <alelkdev@gmail.com>
2022-11-17 19:24:18 +0300

    feat(model): Update psalm model: add references field.

--- scala/library-manager/src/main/scala/model/Psalm.scala
+++ scala/library-manager/src/main/scala/model/Psalm.scala
@@ -4,6 +4,8 @@ package model
 case class Psalm(version: String,
                  name: String,
                  numbers: List[PsalmNumber],
+                 text: List[PsalmPart],
+                 references: List[Reference] = Nil,
                  tonalities: List[Tonality] = Nil,
                  author: Option[String] = None,
                  translator: Option[String] = None,
