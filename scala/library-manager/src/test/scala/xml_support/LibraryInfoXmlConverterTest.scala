7cd6a242826b6e971fff245a5d81352b04e78ebc
Alex Elkin <alelkdev@gmail.com>
2022-11-19 00:13:51 +0300

    test(xml-support): cover library info xml converter with unit tests

---
+++ scala/library-manager/src/test/scala/xml_support/LibraryInfoXmlConverterTest.scala
@@ -0,0 +1,73 @@
+package com.alelk.pws.library_manager
+package xml_support
+
+import model.*
+
+import advxml.data.ValidatedNelThrow
+import advxml.implicits.*
+import cats.data.Validated.Valid
+import com.github.dwickern.macros.NameOf.*
+import io.lemonlabs.uri.RelativeUrl
+import org.scalactic.PrettyPair
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.*
+
+import java.util.Locale
+import scala.xml.PrettyPrinter
+
+class LibraryInfoXmlConverterTest extends AnyFlatSpec with should.Matchers with LibraryInfoXmlConverter {
+
+  nameOf(libraryInfoXmlDecoder) should "parse library info from XML content" in {
+    val xml =
+    // @formatter:off
+      <pwslibrary version='1.2'>
+        <books>
+          <ref preference='40'>books/PV3300.pwsbk</ref>
+          <ref preference='25'>books/PV2001.pwsbk</ref>
+        </books>
+      </pwslibrary>
+      // @formatter:on
+
+    val Valid(libInfo) = xml.decode[LibraryInfo]
+    libInfo.version shouldBe "1.2"
+    libInfo.bookRefs should have length 2
+    val List(ref1, ref2) = libInfo.bookRefs
+    ref1.preference shouldBe 40
+    ref1.reference shouldBe RelativeUrl.parse("books/PV3300.pwsbk")
+    ref2.preference shouldBe 25
+    ref2.reference shouldBe RelativeUrl.parse("books/PV2001.pwsbk")
+  }
+
+  nameOf(libraryInfoXmlEncoder) should "serialize library info to XML" in {
+    val actual =
+      LibraryInfo(
+        version = "1.2",
+        bookRefs = List(
+          BookRef(40, RelativeUrl.parse("books/PV3300.pwsbk")),
+          BookRef(25, RelativeUrl.parse("books/PV2001.pwsbk")),
+        )).encode
+
+    val expected =
+    // @formatter:off
+      <pwslibrary version='1.2'>
+        <books>
+          <ref preference='40'>books/PV3300.pwsbk</ref>
+          <ref preference='25'>books/PV2001.pwsbk</ref>
+        </books>
+      </pwslibrary>
+    // @formatter:on
+    actual.normalize.toString() shouldBe expected.normalize.toString()
+  }
+
+  classOf[LibraryInfoXmlConverter].getSimpleName should "convert library info to XML and parse it back" in {
+    val expected =
+      LibraryInfo(
+        version = "1.2",
+        bookRefs = List(
+          BookRef(40, RelativeUrl.parse("books/PV3300.pwsbk")),
+          BookRef(25, RelativeUrl.parse("books/PV2001.pwsbk")),
+        ))
+    val Valid(actual) = expected.encode.normalize.decode[LibraryInfo]
+    actual shouldBe expected
+  }
+}
