f7db127492877e2ef550d48e80fe02167b662b90
Alex Elkin <elckinne@gmail.com>
2022-11-19 01:08:05 +0300

    test(library-loader): add test for pws library loader

---
+++ scala/library-manager/src/test/scala/PwsLibraryV1xLoaderTest.scala
@@ -0,0 +1,33 @@
+package com.alelk.pws.library_manager
+
+import cats.data.Validated.Valid
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.*
+import com.github.dwickern.macros.NameOf.*
+import io.lemonlabs.uri.Url
+import cats.implicits.*
+
+class PwsLibraryV1xLoaderTest extends AnyFlatSpec with should.Matchers with PwsLibraryV1xLoader {
+
+  nameOf(withLibrary) can "load library content" in {
+    val libFile =
+      Url.parse(
+        classOf[PwsLibraryV1xLoaderTest].getClassLoader
+          .getResource("library-examples-full/content-ru.pwslib").toString)
+    val Valid(resultPsalm) = withLibrary(libFile) { lib =>
+      lib.info.version shouldNot have length 0
+      lib.books.andThen { books =>
+        books should have length 2
+        val List(book1, book2) = books
+        book1.info.displayName shouldNot have length 0
+        book2.info.displayName shouldNot have length 0
+        book2.psalms.andThen { psalms =>
+          psalms should have length 2
+          psalms.head.valid
+        }
+      }
+    }
+    resultPsalm.name shouldNot have length 0
+  }
+
+}
