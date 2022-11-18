e9bc6ea1e6727e6539287975940cf02da7563735
Alex Elkin <alelkdev@gmail.com>
2022-11-19 00:14:48 +0300

    feat(library-loader): implement pws v1.x library loader

---
+++ scala/library-manager/src/main/scala/PwsLibraryV1xLoader.scala
@@ -0,0 +1,42 @@
+package com.alelk.pws.library_manager
+
+import model.{BookInfo, LibraryInfo, Psalm}
+import xml_support.{PsalmNumberXmlConverter, PsalmPartXmlConverter, PsalmXmlConverter, ReferenceXmlConverter, *}
+
+import advxml.data.*
+import advxml.implicits.*
+import advxml.transform.XmlZoom.*
+import cats.syntax.all.*
+import io.lemonlabs.uri.Url
+
+import java.net.{URI, URL}
+import scala.xml.XML
+
+class Library private[library_manager](info: LibraryInfo, url: Url) extends BookInfoXmlConverter {
+  lazy val books: ValidatedNelThrow[List[Book]] =
+    info.bookRefs.map { ref =>
+      val bookUrl = Url.parse(URI.create(url.toString).resolve(ref.reference.toString).toString)
+      XML.load(bookUrl.toString).decode[BookInfo]
+        .map(Book(_, url))
+    }.sequence
+}
+
+class Book private[library_manager](info: BookInfo, url: Url)
+  extends PsalmXmlConverter with PsalmPartXmlConverter with PsalmNumberXmlConverter with ReferenceXmlConverter {
+
+  lazy val psalms: ValidatedNelThrow[List[Psalm]] =
+    info.psalmRefs.map { ref =>
+      val psalmUrl = Url.parse(URI.create(url.toString).resolve(ref.toString).toString)
+      XML.load(psalmUrl.toString).decode[Psalm]
+    }.sequence
+}
+
+trait PwsLibraryV1xLoader extends LibraryInfoXmlConverter {
+
+  def withLibrary[T](url: Url)(f: Library => ValidatedNelThrow[T]): ValidatedNelThrow[T] =
+    XML.load(url.toString)
+      .decode[LibraryInfo]
+      .andThen { libInfo =>
+        f(Library(libInfo, url))
+      }
+}
