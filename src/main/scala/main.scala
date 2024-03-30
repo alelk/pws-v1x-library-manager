b4486bed5688b317ee34d1eea75ba96e43927cb3
Alex Elkin <alelkdev@gmail.com>
2024-03-30 23:07:17 +0300

    feat(converter): implement library converter: v1x -> v2x

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -8,6 +8,8 @@ import io.lemonlabs.uri.Url
 import java.io.File
 
 val pwsLibraryValidator = new PwsLibraryV1xValidator {}
+val pwsLibraryV1xToV2xConverter = new PwsLibraryV1xToV2xConverter {}
+val pwsLibraryV2Writer = new PwsLibraryV2Writer {}
 
 @main
 def main(args: String*): Unit = {
@@ -22,6 +24,21 @@ def main(args: String*): Unit = {
           System.err.println(s"Library is invalid: \n  ${errors.mkString("\n  ")}")
         case Invalid(errors) =>
           System.err.println(s"Error when loading library: \n  ${errors.map(_.getMessage).toList.mkString("\n  ")}")
+
+    case List("convert", filename, outputFilename) =>
+      val file = File(".").toURI.resolve(filename).toURL
+      println(s"Convert PWS library $file to v2.x format")
+      val result = pwsLibraryV1xToV2xConverter.convert(Url.parse(file.toString))
+      result match
+        case Valid(library) =>
+          println(s"Count books: ${library.books.size}")
+          println(s"Count psalms: ${library.books.map(_.book.psalms.size).sum}")
+          val outputUrl = Url.parse(File(".").toURI.resolve(outputFilename).toURL.toString)
+          pwsLibraryV2Writer.write(library, outputUrl)
+          println(s"Library converted and saved to $outputUrl")
+        case Invalid(errors) =>
+          System.err.println(s"Error when converting library: \n  ${errors.map(_.getMessage).toList.mkString("\n  ")}")
+
     case List("--help") =>
       println(
         """
